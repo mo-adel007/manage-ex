8df1e9befc59226956920340098eb351927436ee
mo-adel007 <muhamedadelfahmy99@gmail.com>
2025-07-03 15:32:57 +0300

    .

--- components/GlobalLightningBackground.tsx
+++ components/GlobalLightningBackground.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import Lightning from './Lightning'
-import Hyperspeed from './Hyperspeed'
+import Particles from './Particles'
 import { useTheme } from './ThemeProvider'
 
 export default function GlobalLightningBackground() {
@@ -17,45 +17,6 @@ export default function GlobalLightningBackground() {
     return null
   }
 
-  // Enhanced Hyperspeed options for better visibility in light theme
-  const hyperspeedOptions = {
-    onSpeedUp: () => {},
-    onSlowDown: () => {},
-    distortion: 'turbulentDistortion',
-    length: 400,
-    roadWidth: 14,
-    islandWidth: 4,
-    lanesPerRoad: 4,
-    fov: 90,
-    fovSpeedUp: 150,
-    speedUp: 1.8,
-    carLightsFade: 0.9,
-    totalSideLightSticks: 40,
-    lightPairsPerRoadWay: 60,
-    shoulderLinesWidthPercentage: 0.1,
-    brokenLinesWidthPercentage: 0.15,
-    brokenLinesLengthPercentage: 0.7,
-    lightStickWidth: [0.2, 0.8] as [number, number],
-    lightStickHeight: [1.8, 2.5] as [number, number],
-    movingAwaySpeed: [50, 80] as [number, number],
-    movingCloserSpeed: [-120, -160] as [number, number],
-    carLightsLength: [20, 120] as [number, number],
-    carLightsRadius: [0.1, 0.25] as [number, number],
-    carWidthPercentage: [0.5, 0.8] as [number, number],
-    carShiftX: [-1.2, 1.2] as [number, number],
-    carFloorSeparation: [0, 10] as [number, number],
-    colors: {
-      roadColor: '#dee2e6',
-      islandColor: '#ced4da',
-      background: 'transparent',
-      shoulderLines: '#5b4389',
-      brokenLines: '#8b7cc8',
-      leftCars: ['#5b4389', '#8b7cc8', '#c297c1'],
-      rightCars: ['#6750A2', '#8b7cc8', '#5b4389'],
-      sticks: '#c297c1',
-    }
-  }
-
   return (
     <div className="global-background-effect">
       {theme === 'dark' ? (
@@ -67,7 +28,16 @@ export default function GlobalLightningBackground() {
           size={1.5}
         />
       ) : (
-        <Hyperspeed effectOptions={hyperspeedOptions} />
+        <Particles
+          particleColors={["#2d1856"]}
+          particleCount={500}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
       )}
 
       <style jsx>{`
